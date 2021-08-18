144985d9767017e745fe961d4a298aaeabf5f860
mohamedali <soft.mohamedali@gmail.com>
2021-08-18 04:12:04 +0200

    first commit

---
+++ app/src/main/java/com/example/runfittnes/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.runfittnes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
